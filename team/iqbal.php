f879db52e2e349bf74955948c923671b9764886f
cepotdotid <cepotdotid@gmail.com>
2021-12-06 04:19:01 +0700

    first commit

---
+++ team/iqbal.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <title>RaiGetheg - Single Portfolio</title>
+    <link rel="shortcut icon" type="image/x-icon" href="../images/favicon.png" />
+    <link rel="stylesheet" href="../css/bootstrap.min.css" />
+    <link rel="stylesheet" href="../css/font-awesome.min.css" />
+    <link rel="stylesheet" href="../css/flaticon.css" />
+    <link rel="stylesheet" href="../css/owl.carousel.min.css" />
+    <link rel="stylesheet" href="../css/owl.theme.css" />
+    <link rel="stylesheet" href="../css/magnific-popup.css" />
+    <link rel="stylesheet" href="../css/lightgallery.css" />
+    <link rel="stylesheet" href="../css/woocommerce.css" />
+    <link rel="stylesheet" href="../css/royal-preload.css" />
+
+    <link rel="stylesheet" href="../style.css" />
+</head>
+
+<body>
+    <div id="royal_preloader"></div>
+    <div id="page" class="site">
+        <?php
+        include "../includes/header.php";
+        ?>
+        <div id="side-panel" class="side-panel">
+            <a href="#" class="side-panel-close"><i class="ot-flaticon-close-1"></i></a>
+            <div class="side-panel-block">
+                <div class="side-panel-wrap">
+                    <div class="the-logo">
+                        <a href="index-html.html">
+                            <img src="../images/logo-footer.svg" alt="Theratio">
+                        </a>
+                    </div>
+                    <div class="ot-heading">
+                        <h2 class="main-heading">Our Gallery</h2>
+                    </div>
+                    <div class="image-gallery">
+                        <div id="gallery-1" class="gallery galleryid-102 gallery-columns-3 gallery-size-thumbnail">
+                            <figure class="gallery-item">
+                                <div class="gallery-icon landscape">
+                                    <a data-elementor-open-lightbox="yes" data-elementor-lightbox-slideshow="8701e24" data-elementor-lightbox-title="p4-gallery1" href="images/gallery1.jpg">
+                                        <img src="../images/gallery-small-1.jpg" class="" alt="">
+                                    </a>
+                                </div>
+                            </figure>
+                            <figure class="gallery-item">
+                                <div class="gallery-icon landscape">
+                                    <a data-elementor-open-lightbox="yes" data-elementor-lightbox-slideshow="8701e24" data-elementor-lightbox-title="p4-gallery1" href="images/gallery2.jpg">
+                                        <img src="../images/gallery-small-2.jpg" class="" alt="">
+                                    </a>
+                                </div>
+                            </figure>
+                            <figure class="gallery-item">
+                                <div class="gallery-icon landscape">
+                                    <a data-elementor-open-lightbox="yes" data-elementor-lightbox-slideshow="8701e24" data-elementor-lightbox-title="p4-gallery1" href="images/gallery3.jpg">
+                                        <img src="../images/gallery-small-3.jpg" class="" alt="">
+                                    </a>
+                                </div>
+                            </figure>
+                            <figure class="gallery-item">
+                                <div class="gallery-icon landscape">
+                                    <a data-elementor-open-lightbox="yes" data-elementor-lightbox-slideshow="8701e24" data-elementor-lightbox-title="p4-gallery1" href="images/gallery4.jpg">
+                                        <img src="../images/gallery-small-4.jpg" class="" alt="">
+                                    </a>
+                                </div>
+                            </figure>
+                            <figure class="gallery-item">
+                                <div class="gallery-icon landscape">
+                                    <a data-elementor-open-lightbox="yes" data-elementor-lightbox-slideshow="8701e24" data-elementor-lightbox-title="p4-gallery1" href="images/gallery5.jpg">
+                                        <img src="../images/gallery-small-5.jpg" class="" alt="">
+                                    </a>
+                                </div>
+                            </figure>
+                            <figure class="gallery-item">
+                                <div class="gallery-icon landscape">
+                                    <a data-elementor-open-lightbox="yes" data-elementor-lightbox-slideshow="8701e24" data-elementor-lightbox-title="p4-gallery1" href="images/gallery6.jpg">
+                                        <img src="../images/gallery-small-6.jpg" class="" alt="">
+                                    </a>
+                                </div>
+                            </figure>
+                        </div>
+                    </div>
+                    <div class="ot-heading ">
+                        <h2 class="main-heading">Contact Info</h2>
+                    </div>
+                    <div class="side-panel-cinfo">
+                        <ul class="panel-cinfo">
+                            <li class="panel-list-item">
+                                <span class="panel-list-icon"><i class="ot-flaticon-place"></i></span>
+                                <span class="panel-list-text">411 University St, Seattle, USA</span>
+                            </li>
+                            <li class="panel-list-item">
+                                <span class="panel-list-icon"><i class="ot-flaticon-mail"></i></span>
+                                <span class="panel-list-text">theratio_interior@mail.com</span>
+                            </li>
+                            <li class="panel-list-item">
+                                <span class="panel-list-icon"><i class="ot-flaticon-phone-call"></i></span>
+                                <span class="panel-list-text">+1 800 456 789 123</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="side-panel-social">
+                        <ul>
+                            <li><a href="http://twitter.com/" target="_self"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="http://facebook.com/" target="_self"><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="http://linkedin.com/" target="_self"><i class="fab fa-linkedin-in"></i></a></li>
+                            <li><a href="http://instagram/" target="_self"><i class="fab fa-instagram"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="content" class="site-content">
+            <div class="page-header dtable text-center header-transparent pheader-team">
+                <div class="dcell">
+                    <div class="container">
+                        <h1 class="page-title">Iqbal Portfolio</h1>
+                        <ul id="breadcrumbs" class="breadcrumbs none-style">
+                            <li><a href="index-2.html">Home</a></li>
+                            <li><a href="index-2.html">Our Team</a></li>
+                            <li class="active">Portfolio</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="single-team">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-md-12 text-center align-self-center mb-5 mb-lg-0">
+                        <div class="team-member-img">
+                            <img src="../images/image1-single-team.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-8 col-md-12">
+                        <div class="team-member-info">
+                            <h2>Iqbal Hamzah Fanshuri</h2>
+                            <h5> [ Founder The Company ]</h5>
+                            <ul class="member-info">
+                                <li><span class="text-light">DEPARTMENT:</span> Management Project</li>
+                                <li><span class="text-light">EXPERIENCE:</span> 4 years</li>
+                                <li><span class="text-light">EMAIL:</span> iqbalhmzh@gmail.com</li>
+                                <li><span class="text-light">PHONE:</span> 089 651 135 974</li>
+                            </ul>
+                            <div class="list-sociall">
+                                <ul>
+                                    <li><a href="https://www.facebook.com/Iqbal.badboy/" target="_self"><i class="fab fa-facebook-f"></i></a></li>
+                                    <!-- <li><a href="http://twitter.com/" target="_self"><i class="fab fa-twitter"></i></a></li> -->
+                                    <li><a href="https://www.linkedin.com/in/iqbal-h-fanshuri-416988160/" target="_self"><i class="fab fa-linkedin-in"></i></a></li>
+                                    <li><a href="https://www.instagram.com/iqbalhmzh/" target="_self"><i class="fab fa-instagram"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="single-team-detail">
+                            <h4>Biography</h4>
+                            <p>Iqbal Hamzah has experience in project management for 4 years focusing on residential buildings, Interior, Exterior, Landscape, Hardscape. Many years in residential and public space design — <span><strong>founder and head of production management at RAIGETHEG-Creative engineering.</strong></span> In building the design and build, the concept is based on the theme that is developing at this time. Implement various concepts; Scandinavian, Industrial, Minimalist, Rustic, Tropical and so on. In carrying out a project, the point of interest in each space is the main focus. Realizing the shape of a design is a pleasure. Have a strong determination to develop in their field.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6 col-md-12 mb-4 mb-lg-0">
+                        <div class="row">
+                            <div class="col-md-6 col-sm-12">
+                                <div class="team-feature-detail">
+                                    <h6>RaiGetheg</h6>
+                                    <p>Interior Designer, Illustrator</p>
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-sm-12">
+                                <div class="team-feature-detail">
+                                    <h6>MEDIA GROUP</h6>
+                                    <p>Photographer, Designer</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12">
+                        <div class="ot-progress pb-3" data-percent="60" data-processed="true">
+                            <div class="overflow">
+                                <span class="pname f-left">interior sketch</span>
+                            </div>
+                            <div class="iprogress">
+                                <div class="progress-bar"><span class="ppercent">60%</span></div>
+                            </div>
+                        </div>
+                        <div class="ot-progress pb-3" data-percent="70" data-processed="true">
+                            <div class="overflow"><span class="pname f-left">3D Modeling</span></div>
+                            <div class="iprogress">
+                                <div class="progress-bar"><span class="ppercent">70%</span></div>
+                            </div>
+                        </div>
+                        <div class="ot-progress" data-percent="80" data-processed="true">
+                            <div class="overflow"><span class="pname f-left">2D Planning</span></div>
+                            <div class="iprogress">
+                                <div class="progress-bar"><span class="ppercent">80%</span></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="single-team-skill">
+                            <h4>Hard & Soft Skills</h4>
+                            <p>The basic goal of our studio is to create solutions for people on the move implementing someone in a form. aesthetically pleasing to our customers, with lightning-fast project development using unique styles and architectures. Even if you don't have a sketch of what you want – we'll help you get the result you're dreaming of. We can also help you create a harmony of elements that don't seem to fit at all in an interior or exterior design. Do you want to combine classic and abstract art? Minimalist and industrial? We are ready to make it happen by design and implementation!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <?php
+        include "../includes/footer.php";
+        ?>
+    </div><!-- #page -->
+    <a id="back-to-top" href="#" class="show"><i class="ot-flaticon-left-arrow"></i></a>
+    <!-- jQuery -->
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/mousewheel.min.js"></script>
+    <script src="../js/lightgallery-all.min.js"></script>
+    <script src="../js/jquery.magnific-popup.min.js"></script>
+    <script src="../js/jquery.isotope.min.js"></script>
+    <script src="../js/owl.carousel.min.js"></script>
+    <script src="../js/easypiechart.min.js"></script>
+    <script src="../js/jquery.countdown.min.js"></script>
+    <script src="../js/scripts.js"></script>
+    <script src="../js/royal_preloader.min.js"></script>
+    <script>
+        window.jQuery = window.$ = jQuery;
+        (function($) {
+            "use strict";
+            //Preloader
+            Royal_Preloader.config({
+                mode: 'text',
+                text: 'RaiGetheg Creative',
+                background: '#1a1a1a',
+            });
+        })(jQuery);
+    </script>
+</body>
+
+</html>
